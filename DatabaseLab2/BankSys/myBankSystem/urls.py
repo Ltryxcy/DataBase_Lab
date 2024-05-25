894946b8ce0730cc476fcb7952b2c5227255565b
Ltryxcy <2570258065@qq.com>
2024-05-26 01:38:58 +0800

    register.html

--- DatabaseLab2/BankSys/myBankSystem/urls.py
+++ DatabaseLab2/BankSys/myBankSystem/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 app_name = 'myBankSystem'
@@ -9,6 +9,7 @@ urlpatterns = [
     path('department_staff/<int:department_id>/', views.department_staff, name='department_staff'),
     path('login/', views.bank_customer_login, name='login'),
     path('register/', views.bank_customer_register, name='register'),
+    path('', include('django.contrib.auth.urls')),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
